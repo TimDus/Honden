09d51d43b33174eaea48e2e034533ab3cbfb0ef8
TimDus <timvddussen@gmail.com>
2021-01-20 05:09:31 +0100

    Update PlayerTest.java

--- dograceFX/src/test/java/PlayerTest.java
+++ dograceFX/src/test/java/PlayerTest.java
@@ -1,13 +1,10 @@
-import Animals.Animal;
-import Animals.Gender;
-import Animals.Reservor;
 import dograce.models.Player;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 import java.time.LocalDateTime;
 
-public class AnimalTest
+public class PlayerTest
 {
     @Test
     void TestConstructor()
@@ -18,7 +15,7 @@ public class AnimalTest
         String password = "wachtwoord";
 
         boolean result = false;
-        if(animal.getName().equals(name) && animal.getGender().equals(gender) && animal.getPrice().equals(price))
+        if(player.getPlayerName().equals(name) && player.getPassword().equals(password))
         {
             result = true;
         }
